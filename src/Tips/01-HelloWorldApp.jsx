0339dcbb51969fdab0760fb98ff66102a64a90a4
Nicolas Vidal <nico.vidal@duocuc.cl>
2022-09-28 14:56:28 -0300

    tips

---
+++ src/Tips/01-HelloWorldApp.jsx
@@ -0,0 +1,17 @@
+/* export function App(){
+
+    return(<h1>Hola Mundo!!</h1>)
+
+} */
+
+//tambien puede ser por defecto
+
+//export default App;
+
+//usando HelloWorldApp en el componente
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>HelloWorldApp</h1>
+  );
+}
