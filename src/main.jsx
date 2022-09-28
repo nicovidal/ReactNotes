f6d88cc7c2785ebb5fc35b258f72ee148e61fc01
Nicolas Vidal <nico.vidal@duocuc.cl>
2022-09-28 17:15:47 -0300

    props

--- src/main.jsx
+++ src/main.jsx
@@ -3,27 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp';
 import { HelloWorldApp } from './HelloWorldApp';
-
-//importar el componente
-//import { App} from './HelloWorldApp'
+import './styles.css';
 
 
-
-//todo react comienza con un componente no es mas que una funcion
-//function alt components 
-
-//function App(){
-    //document.createElement...
-    //estoe s una forma sensilla
-    //return(<h1>Hola Mundo!!</h1>)
-
-//}
-
-
-//ahora lo renderizamos
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp/>
+        <FirstApp title="Hola, soy goku "subTitle={123}/>
     </React.StrictMode>
 )
