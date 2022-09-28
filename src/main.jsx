a430aaa42f097fbff1cbc1533d758625388c79b6
Nicolas Vidal <nico.vidal@duocuc.cl>
2022-09-28 14:01:20 -0300

    primer componente

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,27 @@
 //elementos para renderizar la app
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelloWorldApp } from './HelloWorldApp';
+//importar el componente
+//import { App} from './HelloWorldApp'
+
+
 
 //todo react comienza con un componente no es mas que una funcion
 //function alt components 
 
-function App(){
+//function App(){
     //document.createElement...
     //estoe s una forma sensilla
-    return(<h1>Hola Mundo!!</h1>)
+    //return(<h1>Hola Mundo!!</h1>)
 
-}
+//}
+
 
 //ahora lo renderizamos
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App/>
+        <HelloWorldApp/>
     </React.StrictMode>
 )
