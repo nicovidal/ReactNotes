a91a8b8c3beb99f593269a30cb4c7157b7b2da10
Nicolas Vidal <nico.vidal@duocuc.cl>
2022-09-28 14:56:24 -0300

    tips variable html

---
+++ src/Tips/01-main.jsx
@@ -0,0 +1,29 @@
+//elementos para renderizar la app
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { FirstApp } from './01-FirstApp';
+import { HelloWorldApp } from './01-HelloWorldApp';
+
+//importar el componente
+//import { App} from './HelloWorldApp'
+
+
+
+//todo react comienza con un componente no es mas que una funcion
+//function alt components 
+
+//function App(){
+    //document.createElement...
+    //estoe s una forma sensilla
+    //return(<h1>Hola Mundo!!</h1>)
+
+//}
+
+
+//ahora lo renderizamos
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <FirstApp/>
+    </React.StrictMode>
+)
