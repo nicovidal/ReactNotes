087c9d5e54a2731f82464402f80802705fbbc8f9
Nicolas Vidal <nico.vidal@duocuc.cl>
2022-09-29 21:17:39 -0300

    hook

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,6 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <CounterApp value={ 123}/> 
+        <CounterApp value={ 20}/> 
     </React.StrictMode>
 )
